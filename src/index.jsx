ba8b4c43ab1cab4af5d84cd8df49b11296695d8a
Certsoftt <ukpeubong8573@gmail.com>
2025-04-18 10:51:12 +0100

    Add: movie api get request

--- src/index.jsx
+++ src/index.jsx
@@ -36,5 +36,20 @@ Get.prototype.api = async function (endpoint="",params=""){
             console.log(`set repo username as it is a required argument`)
         } 
     }
+    if(this.input == "movies"){
+        if(params === "" && endpoint === ""){
+            this.response = await fetch(`https://yts.mx/api/v2/movie_details.json`)
+            this.data = this.response.json().then(result=>{
+                setFetch(result)
+            })
+        }else if(endpoint !== ""){
+            this.response = await fetch(`https://yts.mx/api/v2/movie_details.json/${endpoint}`)
+            this.data = this.response.json().then(result=>{
+                setFetch(result)
+            })
+        }else{
+            console.log(`Error in endpoint query params. Re-check endpoints params`)
+        } 
+    }
     return fetch
 }
